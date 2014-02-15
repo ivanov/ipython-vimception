3c9c5f95c0d5b46b8dab3eba4d90c246c65f2a4e
Paul Ivanov <pi@berkeley.edu>
2014-02-14 16:50:48 -0800

    here's the vimception magic

---
+++ vimception.py
@@ -0,0 +1,26 @@
+"""
+This is VimCeption
+
+When you load this extension, you will feel a kick
+
+"""
+import os
+from IPython.display import display, Javascript
+
+fname = os.path.join(os.path.dirname(__file__), 'vimception.js')
+with open(fname) as f:
+    vimception_js = f.read()
+
+def vimception(line=''):
+    with open(fname) as f:
+        vimception_js = f.read()
+    display(Javascript(vimception_js))
+    if line != 'off':
+        display(Javascript(vimception_js))
+    else:
+        print line
+        display(Javascript(vimception_js + "to('default');"));
+
+def load_ipython_extension(ip):
+    vimception()
+    ip.magics_manager.register_function(vimception)
